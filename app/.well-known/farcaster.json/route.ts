9c7c3dae01d3938682cf6ee91f145e9750867eb2
AzrielTheHellrazor <goktug200402@gmail.com>
2025-09-18 01:51:09 +0300

    update: farcaster API

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,6 +1,7 @@
-import { withValidManifest } from "@coinbase/onchainkit/minikit";
-import { minikitConfig } from "../../../minikit.config";
-
 export async function GET() {
-  return Response.json(withValidManifest(minikitConfig));
+  // Redirect to Farcaster hosted manifest
+  return Response.redirect(
+    "https://api.farcaster.xyz/miniapps/hosted-manifest/019959de-9ed2-e882-fe2a-4acd4f9ae5c1",
+    307
+  );
 }
